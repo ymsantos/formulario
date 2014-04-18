fa12d9fc3bfed97faeb1e99a7a9c7f55f094bf00
Jonas <jonas.jmsantos@gmail.com>
2014-04-18 19:51:44 -0300

    ultima versao so far

--- recuperaSenha.php
+++ recuperaSenha.php
@@ -21,17 +21,6 @@ $hash = $dados['senha'];
 if (mysql_num_rows($rs)){
     //include("sucessoSenha.php");
     
-    // $from = "From: jonas.jmsantos@gmail.com";
-    // $to = "jonas.jms@live.com";
-    // $subject = "PPGCM - Recuperação da Senha";
-    // $message = "Testando a email para recuperar a senha do PPGCM";
-    
-    // ini_set('SMTP',localhost);
-    // ini_set('smtp_port',25);
-    // ini_set('send_mail_from','jonas.jmsantos@gmail.com');
-    
-    // mail($to, $subject, $message, $from, "-r");
-    
     $email = new MailSender;
     
     $emailDest = (string)$dados["email"];
